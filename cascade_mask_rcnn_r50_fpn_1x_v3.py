a16ad8f0461beff8e9f9c498376bc03b80b427c7
Celestenono <noemie.moreau96@gmail.com>
2023-10-23 11:41:37 +0200

    correct glom en version 3

--- cascade_mask_rcnn_r50_fpn_1x_v3.py
+++ cascade_mask_rcnn_r50_fpn_1x_v3.py
@@ -127,7 +127,7 @@ model = dict(
         rpn_proposal=dict(
             nms_pre=2000,
             max_per_img=2000,
-            nms=dict(type=nms, iou_threshold=0.7),
+            nms=dict(type="nms", iou_threshold=0.7),
             min_bbox_size=0),
         rcnn=[
             dict(
